8805a89de3f450d1100a00f34b784cbd97bd9d4b
CHANIVER <cksdlqj@gmail.com>
2024-10-24 21:43:29 +0900

    #4.9 Login Form

--- lib/features/authentication/login_form_screen.dart
+++ lib/features/authentication/login_form_screen.dart
@@ -1,10 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone_again/constants/gaps.dart';
+import 'package:tiktok_clone_again/constants/sizes.dart';
+import 'package:tiktok_clone_again/features/authentication/widgets/form_button.dart';
 
-class LoginFormScreen extends StatelessWidget {
+class LoginFormScreen extends StatefulWidget {
   const LoginFormScreen({super.key});
 
   @override
+  State<LoginFormScreen> createState() => _LoginFormScreenState();
+}
+
+class _LoginFormScreenState extends State<LoginFormScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  void _onSubmitTap() {
+    if (_formKey.currentState != null) {
+      if (_formKey.currentState!.validate()) {
+        _formKey.currentState?.save();
+      }
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Log in'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: Sizes.size40,
+        ),
+        child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'Email',
+                  ),
+                  validator: (value) {
+                    return null;
+                  },
+                  onSaved: (newValue) => print(newValue),
+                ),
+                Gaps.v16,
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'Password',
+                  ),
+                ),
+                Gaps.v28,
+                GestureDetector(
+                  onTap: _onSubmitTap,
+                  child: const FormButton(disable: false),
+                ) // Code Challenge text:
+              ],
+            )),
+      ),
+    );
   }
 }
